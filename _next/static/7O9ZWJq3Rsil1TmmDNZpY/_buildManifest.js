1bce8bee1db7cc21d5fcd69c8070877ea7607964
spaceshiptrip <4379326+spaceshiptrip@users.noreply.github.com>
2024-09-15 20:37:59 -0700

    Updates

--- _next/static/7O9ZWJq3Rsil1TmmDNZpY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-e20e236c909c4cd5.js","static/chunks/pages/index-ce0c8fa797af8450.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
